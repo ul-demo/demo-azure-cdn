aae8834bd0b5fc0491340e6c94db97557148c703
frpol9 <frederic.poliquin@dti.ulaval.ca>
2020-11-03 09:26:30 -0500

    Ajout config jest

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel"
+  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  reporters: [
+    "default",
+    [
+      "./node_modules/jest-html-reporter",
+      {
+        pageTitle: "Test Report",
+        outputPath: "reports/test-report.html",
+      },
+    ],
+  ],
 };
