dd65e576d1bf5b99742b54329eaa3d02af7d5eac
frpol9 <frederic.poliquin@dti.ulaval.ca>
2020-12-15 21:13:19 -0500

    Ajout du support du 404

---
+++ src/components/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    404 - Not Found
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class NotFound extends Vue {
+}
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 
+import NotFound from "../components/NotFound.vue";
 import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
@@ -19,6 +20,11 @@ const routes: Array<RouteConfig> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "*",
+    name: "404",
+    component: NotFound,
   },
 ];
 
